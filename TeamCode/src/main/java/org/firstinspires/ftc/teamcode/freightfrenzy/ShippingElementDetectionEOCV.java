b993272fe51b5d5d46100d267c1d80375227dc65
wilcoxrobotics <teamrobotics8872@gmail.com>
2022-02-03 16:33:46 -0800

    added more to pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/ShippingElementDetectionEOCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/ShippingElementDetectionEOCV.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.freightfrenzy;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -9,17 +9,15 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 @Autonomous
-public class ShippingElementDetectionEOCV extends LinearOpMode {
-    ShippingElementPipeline pipeline;
-    WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
-
+public class ShippingElementDetectionEOCV extends OpMode {
     OpenCvWebcam webcam;
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-        webcam.setPipeline(pipeline);
+        webcam.setPipeline(new ShippingElementPipeline());
 
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
@@ -35,29 +33,10 @@ public class ShippingElementDetectionEOCV extends LinearOpMode {
                  */
             }
         });
+    }
 
+    @Override
+    public void loop() {
 
-        telemetry.addLine("Waiting for start");
-        telemetry.update();
-
-        /*
-         * Wait for the user to press start on the Driver Station
-         */
-        waitForStart();
-
-        while (opModeIsActive()) {
-            /*
-             * Send some stats to the telemetry
-             */
-            telemetry.addData("Frame Count", webcam.getFrameCount());
-            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
-            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
-            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
-            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
-            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
-            telemetry.update();
-
-
-        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/ShippingElementPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/ShippingElementPipeline.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.freightfrenzy;
 
+import com.acmerobotics.dashboard.config.Config;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+@Config
 public class ShippingElementPipeline extends OpenCvPipeline {
     enum ShippingElementPosition {
         LEFT,
@@ -11,13 +14,17 @@ public class ShippingElementPipeline extends OpenCvPipeline {
         RIGHT
     }
 
-    Mat grey = new Mat();
+    Mat YCbCrMat = new Mat();
+    Mat CbMat = new Mat();
+
+    public static int channel = 2;
 
     @Override
     public Mat processFrame(Mat input) {
+        Imgproc.cvtColor(input, YCbCrMat, Imgproc.COLOR_RGBA2BGR);
+        Imgproc.cvtColor(YCbCrMat, YCbCrMat, Imgproc.COLOR_BGR2YCrCb);
+        Core.extractChannel(YCbCrMat, CbMat, channel);
 
-        Imgproc.cvtColor(input, grey, Imgproc.COLOR_RGB2GRAY);
-
-        return grey;
+        return CbMat;
     }
 }
